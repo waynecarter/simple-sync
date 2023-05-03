40375a019ae672e937ba313dcab2b330b9f3fd0c
Wayne Carter <wayneacarter@hotmail.com>
2023-05-03 00:19:07 -0400

    Remove obsolete comment.

--- simple-p2p/ViewController.swift
+++ simple-p2p/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         // Set up the database and collection.
         let database = try! Database(name: "store")
